4d751f5d0cc3a77c25d204595e2c61581d7b9883
Nicolae Nicora <n.nicora@sap.com>
2025-08-10 14:39:53 +0200

    fix: introduce a flag to explicitly enable the JWT Authentication envoy configuration when there a no JWT providers

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -2,4 +2,7 @@ package flags
 
 const (
 	DisableJWTProviderComputation = "disable-jwt-provider-computation"
+	// EnableAllowMissingJwtAuthenticationEnvoy If the flag has true value will result to add AllowMissingOrFailed on
+	//Envoy JWT Authentication as configuration
+	EnableAllowMissingJwtAuthenticationEnvoy = "enable-allow-missing-jwt-authentication-envoy"
 )
